6983fa2bce81bf97f2b48671e7aea01280c1fec4
Graeme Zinck <graeme.zinck@gmail.com>
2021-10-31 14:03:43 -0400

    Configure ts, eslint, husky, prettier

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+      ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'no-var': 'error',
+    'brace-style': 'error',
+    'prefer-template': 'error',
+    radix: 'error',
+    'space-before-blocks': 'error',
+    'import/prefer-default-export': 'off',
+  },
+};
